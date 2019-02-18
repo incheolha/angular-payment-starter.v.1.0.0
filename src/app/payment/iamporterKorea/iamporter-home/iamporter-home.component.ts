5a19d2cda3555d53ebcd7c6b79edaf4652ae80d2
incheolha <incheolha@gmail.com>
2019-02-19 01:39:50 +0900

    2019.2.15. updated

--- src/app/payment/iamporterKorea/iamporter-home/iamporter-home.component.ts
+++ src/app/payment/iamporterKorea/iamporter-home/iamporter-home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IamporterHomeComponent implements OnInit {
 
+  tableData: object[] = [
+    { first: 'Mark MarkMarkMarkMarkMarkMarkccccccccccccccccccccc' , last: 'Otto', username: '@mdo', email: 'markotto@gmail.com', country: 'USA', city: 'San Francisco' },
+    { first: 'Jacob', last: 'Thornton', username: '@fat', email: 'jacobt@gmail.com', country: 'France', city: 'Paris' },
+    { first: 'Larry', last: 'the Bird', username: '@twitter', email: 'larrybird@gmail.com', country: 'Germany', city: 'Berlin' },
+    { first: 'Paul', last: 'Topolski', username: '@P_Topolski', email: 'ptopolski@gmail.com', country: 'Poland', city: 'Warsaw' },
+    { first: 'Anna', last: 'Doe', username: '@andy', email: 'annadoe@gmail.com', country: 'Spain', city: 'Madrid' }
+  ];
   constructor() { }
 
   ngOnInit() {
